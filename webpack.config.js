3efb924774d086be2f5e1b22f8fbde2fee63d29f
Arno Gourdol <arno@arno.org>
2017-06-14 17:10:52 -0700

    Support for bundling to dist/ via webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: path.resolve(__dirname, "src/mathlive.js"),
@@ -8,6 +9,43 @@ module.exports = {
     library: 'MathLive'
   },
   resolve: {
-    modules: [path.resolve(__dirname, "core"), "editor"]
-  }
+    alias: {
+      "mathlive/core": path.resolve(__dirname, "src/core"),
+      "mathlive/editor": path.resolve(__dirname, "src/editor"),
+      "mathlive/addons": path.resolve(__dirname, "src/addons"),
+    }
+  },
+  module: {
+      loaders: [{
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+      }]
+  },
+  plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+          "screw-ie8": true,
+          compress: {
+          booleans: true,
+          cascade: true,
+          comparisons: true,
+          conditionals: true,
+          dead_code: true,
+          drop_console: true,
+          drop_debugger: true,
+          evaluate: true,
+          if_return: true,
+          join_vars: true,
+          loops: true,
+          pure_getters: true,
+          sequences: true,
+          unsafe: true,
+          unused: true,
+          warnings: false
+        },
+        output: {
+          comments: false,
+        },
+      }),
+  ]
 };
