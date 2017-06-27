02f96f9d1235765fc4135788445b0c4351e70b1c
Arno Gourdol <arno@arno.org>
2017-06-27 08:47:45 -0700

    Added example using require.js to load the debug source files

---
+++ examples/with_requirejs/main.js
@@ -0,0 +1,11 @@
+
+define(['jquery', 'mathlive/mathlive'], 
+    function($, MathLive) {
+    // Render all the static math in the document
+    MathLive.renderMathInDocument({ignoreClass: "mathfield"});
+
+    const mf1 = MathLive.makeMathField(document.getElementById('mf1'));
+
+    // As a shortcut, makeMathField can be passed the ID of the element
+    const mf2 = MathLive.makeMathField('mf2');
+});
