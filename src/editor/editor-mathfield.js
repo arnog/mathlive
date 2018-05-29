e5b50a95bfd7e61c1b152e82c2463d1100c07f80
Arno Gourdol <arno@arno.org>
2018-05-28 18:45:35 -0700

    Improved cross-browser accessibility support

--- src/editor/editor-mathfield.js
+++ src/editor/editor-mathfield.js
@@ -146,7 +146,6 @@ function MathField(element, config) {
         } else {
             markup += `<span style="width: 21px; margin-top: 5px;"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M528 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h480c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm16 336c0 8.823-7.177 16-16 16H48c-8.823 0-16-7.177-16-16V112c0-8.823 7.177-16 16-16h480c8.823 0 16 7.177 16 16v288zM168 268v-24c0-6.627-5.373-12-12-12h-24c-6.627 0-12 5.373-12 12v24c0 6.627 5.373 12 12 12h24c6.627 0 12-5.373 12-12zm96 0v-24c0-6.627-5.373-12-12-12h-24c-6.627 0-12 5.373-12 12v24c0 6.627 5.373 12 12 12h24c6.627 0 12-5.373 12-12zm96 0v-24c0-6.627-5.373-12-12-12h-24c-6.627 0-12 5.373-12 12v24c0 6.627 5.373 12 12 12h24c6.627 0 12-5.373 12-12zm96 0v-24c0-6.627-5.373-12-12-12h-24c-6.627 0-12 5.373-12 12v24c0 6.627 5.373 12 12 12h24c6.627 0 12-5.373 12-12zm-336 80v-24c0-6.627-5.373-12-12-12H84c-6.627 0-12 5.373-12 12v24c0 6.627 5.373 12 12 12h24c6.627 0 12-5.373 12-12zm384 0v-24c0-6.627-5.373-12-12-12h-24c-6.627 0-12 5.373-12 12v24c0 6.627 5.373 12 12 12h24c6.627 0 12-5.373 12-12zM120 188v-24c0-6.627-5.373-12-12-12H84c-6.627 0-12 5.373-12 12v24c0 6.627 5.373 12 12 12h24c6.627 0 12-5.373 12-12zm96 0v-24c0-6.627-5.373-12-12-12h-24c-6.627 0-12 5.373-12 12v24c0 6.627 5.373 12 12 12h24c6.627 0 12-5.373 12-12zm96 0v-24c0-6.627-5.373-12-12-12h-24c-6.627 0-12 5.373-12 12v24c0 6.627 5.373 12 12 12h24c6.627 0 12-5.373 12-12zm96 0v-24c0-6.627-5.373-12-12-12h-24c-6.627 0-12 5.373-12 12v24c0 6.627 5.373 12 12 12h24c6.627 0 12-5.373 12-12zm96 0v-24c0-6.627-5.373-12-12-12h-24c-6.627 0-12 5.373-12 12v24c0 6.627 5.373 12 12 12h24c6.627 0 12-5.373 12-12zm-96 152v-8c0-6.627-5.373-12-12-12H180c-6.627 0-12 5.373-12 12v8c0 6.627 5.373 12 12 12h216c6.627 0 12-5.373 12-12z"/></svg></span>`;
         }
-        markup += '<span class="sr-only">Toggle Virtual Keyboard.</span>';
         markup += '</button>';
     } else {
         markup += '<span ></span>';
@@ -697,6 +696,7 @@ MathField.prototype._announce = function(command, oldMathlist, atomsToSpeak) {
     const ariaLiveChangeHack = /\u00a0/.test(this.ariaLiveText.textContent) ? 
         ' \u202f ' : ' \u00a0 ';
     this.ariaLiveText.textContent = liveText + ariaLiveChangeHack;
+    // this.textarea.setAttribute('aria-label', liveText + ariaLiveChangeHack);
 }
 
 MathField.prototype._onFocus = function() {
@@ -1706,7 +1706,7 @@ MathField.prototype._attachButtonHandlers = function(el, command) {
         }
         el.classList.remove('pressed');
         el.classList.add('active');
-        if (ev.type === 'click' && (ev.clientX !== 0 || ev.clientY !== 0)) {
+        if (ev.type === 'click' && ev.detail !== 0) {
             // This is a click event triggered by a mouse interaction
             // (and not a keyboard interaction)
             // Ignore it, we'll handle the mouseup (or touchend) instead.
