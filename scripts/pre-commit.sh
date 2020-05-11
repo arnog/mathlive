06b13ab8f0f4e7860ea3f8d70e8b8cbbffcd6597
Arno Gourdol <arno@arno.org>
2020-05-10 17:33:06 -0700

    Check prod build on commit, not on push

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,9 +1,6 @@
-#!/bin/bash
+#! bash
 
 set -e  # exit immediately on error
-set -o nounset   # abort on unbound variable
-set -o pipefail  # don't hide errors within pipes
-# set -x    # for debuging, trace what is being executed.
 
 # 
 if [ -f "dist/DEVELOPMENT-BUILD" ]; then
@@ -12,6 +9,3 @@ if [ -f "dist/DEVELOPMENT-BUILD" ]; then
     echo "Run `npm run build production` to update dist/"
     git reset dist/
 fi
-
-# Run test suite
-npx jest
