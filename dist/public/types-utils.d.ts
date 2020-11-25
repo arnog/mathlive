a738467b32d86ff16d1b142e9dbf392362003d89
arnog <arno@arno.org>
2020-11-25 11:20:35 +0100

    fix #707: remove /dist folder from repo

--- dist/public/types-utils.d.ts
+++
@@ -1,11 +0,0 @@
-/* 0.59.0 *//**
- * @internal
- */
-declare type Filter<T, Cond, U extends keyof T = keyof T> = {
-    [K in U]: T[K] extends Cond ? K : never;
-}[U];
-/**
- * @internal
- */
-export declare type Keys<T> = Filter<T, (...args: any[]) => any> & string;
-export {};
--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -2,9 +2,3 @@
 
 set -e  # exit immediately on error
 
-# 
-if [ -f "dist/DEVELOPMENT-BUILD" ]; then
-    # There's a "DEVELOPMENT-BUILD" sentinel file in dist
-    echo "Removing dist/ from commit list"
-    git reset dist/
-fi
