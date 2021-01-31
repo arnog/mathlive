a5feebb596648b21640b87fdc73d2fa95b863981
arnog <arno@arno.org>
2021-01-31 22:00:52 +0100

    feat: more descriptive console messages when there's a problem with the fonts directory

--- src/core/fonts.ts
+++ src/core/fonts.ts
@@ -1,29 +1,37 @@
 import { gScriptUrl } from '../common/script-url';
 import { ErrorListener, MathfieldErrorCode } from '../public/core';
 
-// The URL of the bundled MathLive library. Used later to locate the `fonts`
-// directory, relative to the library
+function resolveRelativeUrl(url: string): string {
+  let result = '';
+  try {
+    result = new URL(url, gScriptUrl).toString();
+  } catch (e) {
+    console.error(
+      `Invalid font directory "${url}" (relative to "${gScriptUrl}")`
+    );
+  }
+  return result;
+}
 
-// If loaded via a <script> tag, `document.currentScript.src` is this location
-// If loaded via a module (e.g. `import ...`),`import.meta.url` is this location.
-// However, `import.meta` is not supported by WebPack. So, use a super-hacky-alternative
-// to get the URL.
-// See https://github.com/webpack/webpack/issues/6719
+function makeFontFace(
+  name: string,
+  source: string,
+  descriptors: Record<string, string> = {}
+): FontFace {
+  return new FontFace(
+    name,
+    `url(${source}.woff2) format('woff2'), url(${source}.woff) format('woff')`,
+    descriptors
+  );
+}
 
 export async function loadFonts(
   fontsDirectory?: string,
   onError?: ErrorListener<MathfieldErrorCode>
 ): Promise<void> {
-  function makeFontFace(
-    name: string,
-    source: string,
-    descriptors: Record<string, string> = {}
-  ): FontFace {
-    return new FontFace(
-      name,
-      `url(${source}.woff2) format('woff2'), url(${source}.woff) format('woff')`,
-      descriptors
-    );
+  // If we're already loading the fonts, we're done.
+  if (document.body.classList.contains('ML__fonts-loading')) {
+    return;
   }
 
   // If the "mathlive-fonts.css" stylesheet is included in the <head> of the
@@ -50,7 +58,6 @@ export async function loadFonts(
       'KaTeX_Size3',
       'KaTeX_Size4',
     ];
-    // For (const fontFace of document.fonts.values()) { console.log(fontFace.family)}
     let fontsLoaded = false;
 
     // Firefox returns true for fonts that are not loaded...
@@ -66,72 +73,68 @@ export async function loadFonts(
       }
     }
 
-    if (!fontsLoaded) {
-      if (document.body.classList.contains('ML__fonts-loading')) {
-        return;
-      }
+    if (fontsLoaded) return;
 
-      document.body.classList.add('ML__fonts-loading');
+    // Locate the `fonts` folder relative to the script URL
+    const fontsFolder = resolveRelativeUrl(fontsDirectory ?? './fonts');
+    if (!fontsFolder) return;
 
-      // Locate the `fonts` folder relative to the script URL
-      const fontsFolder = new URL(
-        fontsDirectory ?? './fonts',
-        gScriptUrl
-      ).toString();
-      const fonts: FontFace[] = ([
-        ['KaTeX_Main-Regular'],
-        ['KaTeX_Main-BoldItalic', { style: 'italic', weight: 'bold' }],
-        ['KaTeX_Main-Bold', { weight: 'bold' }],
-        ['KaTeX_Main-Italic', { style: 'italic' }],
-        ['KaTeX_Math-Italic', { style: 'italic' }],
-        ['KaTeX_Math-BoldItalic', { style: 'italic', weight: 'bold' }],
-        ['KaTeX_AMS-Regular'],
-        ['KaTeX_Caligraphic-Regular'],
-        ['KaTeX_Caligraphic-Bold', { weight: 'bold' }],
-        ['KaTeX_Fraktur-Regular'],
-        ['KaTeX_Fraktur-Bold', { weight: 'bold' }],
-        ['KaTeX_SansSerif-Regular', { style: 'italic' }],
-        ['KaTeX_SansSerif-Bold', { weight: 'bold' }],
-        ['KaTeX_SansSerif-Italic', { style: 'italic' }],
-        ['KaTeX_Script-Regular'],
-        ['KaTeX_Typewriter-Regular'],
-        ['KaTeX_Size1-Regular'],
-        ['KaTeX_Size2-Regular'],
-        ['KaTeX_Size3-Regular'],
-        ['KaTeX_Size4-Regular'],
-      ] as [string, Record<string, string>][]).map((x) =>
-        makeFontFace(
-          x[0].replace(/-[a-zA-Z]+$/, ''),
-          fontsFolder + '/' + x[0],
-          x[1]
-        )
+    document.body.classList.add('ML__fonts-loading');
+
+    const fonts: FontFace[] = ([
+      ['KaTeX_Main-Regular'],
+      ['KaTeX_Main-BoldItalic', { style: 'italic', weight: 'bold' }],
+      ['KaTeX_Main-Bold', { weight: 'bold' }],
+      ['KaTeX_Main-Italic', { style: 'italic' }],
+      ['KaTeX_Math-Italic', { style: 'italic' }],
+      ['KaTeX_Math-BoldItalic', { style: 'italic', weight: 'bold' }],
+      ['KaTeX_AMS-Regular'],
+      ['KaTeX_Caligraphic-Regular'],
+      ['KaTeX_Caligraphic-Bold', { weight: 'bold' }],
+      ['KaTeX_Fraktur-Regular'],
+      ['KaTeX_Fraktur-Bold', { weight: 'bold' }],
+      ['KaTeX_SansSerif-Regular', { style: 'italic' }],
+      ['KaTeX_SansSerif-Bold', { weight: 'bold' }],
+      ['KaTeX_SansSerif-Italic', { style: 'italic' }],
+      ['KaTeX_Script-Regular'],
+      ['KaTeX_Typewriter-Regular'],
+      ['KaTeX_Size1-Regular'],
+      ['KaTeX_Size2-Regular'],
+      ['KaTeX_Size3-Regular'],
+      ['KaTeX_Size4-Regular'],
+    ] as [string, Record<string, string>][]).map((x) =>
+      makeFontFace(
+        x[0].replace(/-[a-zA-Z]+$/, ''),
+        fontsFolder + '/' + x[0],
+        x[1]
+      )
+    );
+    try {
+      const loadedFonts = ((await Promise.all(
+        fonts.map((x) => {
+          try {
+            return x.load();
+          } catch {}
+
+          return undefined;
+        })
+      )) as unknown) as FontFace[];
+      // Render them at the same time
+      loadedFonts.forEach((font) => document.fonts.add(font));
+    } catch (error: unknown) {
+      console.error(
+        `The mathlive fonts could not be loaded from "${fontsFolder}" (${error})`
       );
-      try {
-        const loadedFonts = ((await Promise.all(
-          fonts.map((x) => {
-            try {
-              return x.load();
-            } catch (error: unknown) {
-              if (typeof onError === 'function') {
-                onError({
-                  code: 'font-not-found',
-                  arg: error as string,
-                });
-              }
-            }
-
-            return undefined;
-          })
-        )) as unknown) as FontFace[];
-        // Render them at the same time
-        loadedFonts.forEach((font) => document.fonts.add(font));
-      } catch (error: unknown) {
-        console.error(error);
+      if (typeof onError === 'function') {
+        onError({
+          code: 'font-not-found',
+          arg: error as string,
+        });
       }
-
-      // Event if an error occur, give up and pretend the fonts are
-      // loaded (displayign somehting is better than nothing)
-      document.body.classList.remove('ML__fonts-loading');
     }
+
+    // Event if an error occur, give up and pretend the fonts are
+    // loaded (displaying something is better than nothing)
+    document.body.classList.remove('ML__fonts-loading');
   }
 }
