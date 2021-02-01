df758c648a66af3b46a92b8b042c5108570e77fd
arnog <arno@arno.org>
2021-02-01 09:54:39 +0100

    feat: more explicit error messages when local script URL cannot be determined

--- src/common/script-url.ts
+++ src/common/script-url.ts
@@ -4,7 +4,13 @@ function getFileUrl() {
     .replace(/^Error.*\n/, '')
     .split('\n');
 
-  if (stackTraceFrames.length === 0) return '';
+  if (stackTraceFrames.length === 0) {
+    console.error(
+      'Unable to determine source file location (invalid stack trace).\n' +
+        'Relative paths to specify assets location are not supported.'
+    );
+    return '';
+  }
 
   // 0 = this getFileUrl frame (because the Error is created here)
   // 1 = the caller of getFileUrl (the file path we want to grab)
@@ -12,7 +18,13 @@ function getFileUrl() {
 
   // Extract the script's complete url
   const m = callerFrame.match(/http.*js/);
-  if (!m) return '';
+  if (!m) {
+    console.error(
+      'Unable to determine source file location (unexpected file URL).\n' +
+        'Relative paths to specify assets location are not supported.'
+    );
+    return '';
+  }
   return m[0];
 }
 
@@ -21,9 +33,13 @@ function getFileUrl() {
 
 // If loaded via a <script> tag, `document.currentScript.src` is this location
 // If loaded via a module (e.g. `import ...`),`import.meta.url` is this location.
-// However, `import.meta` is not supported by WebPack. So, use a super-hacky-alternative
-// to get the URL.
+// However, `import.meta` is not supported by WebPack. So, use a
+// super-hacky-alternative to get the URL.
 // See https://github.com/webpack/webpack/issues/6719
 
+// Note that in some circumstances, document.currentScript.src can be ""
+// (the empty string). Therefore, use the "||" operator rather than "??"
+// to properly apply the alternative value in this case.
+
 export const gScriptUrl =
-  (document.currentScript as HTMLScriptElement)?.src ?? getFileUrl();
+  (document.currentScript as HTMLScriptElement)?.src || getFileUrl();
