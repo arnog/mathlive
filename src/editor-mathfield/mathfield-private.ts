6abfa3830d2eb82f9bf75af7a227c99062e8052d
Alessandro Baldo <alessandro.baldo@maieuticallabs.it>
2021-10-20 17:40:45 +0200

    fix: fix #1087 invoke `scrollIntoView` in `insert` commands
    Commands exectued using `insert` didn't update the scroll
    position like command coming from keybindings and typed text.

--- src/editor-mathfield/mathfield-private.ts
+++ src/editor-mathfield/mathfield-private.ts
@@ -972,6 +972,10 @@ export class MathfieldPrivate implements Mathfield {
         void this.keypressSound?.play().catch(console.warn);
       }
 
+      if (options.scrollIntoView) {
+        this.scrollIntoView();
+      }
+
       if (s === '\\\\') {
         // This string is interpreted as an "insert row after" command
         addRowAfter(this.model);
