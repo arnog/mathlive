5a5560b3c046d04b00d077328d81144e845b424d
Arno Gourdol <arno@arno.org>
2022-12-01 05:52:58 +1300

    fix: calculate height/depth based on content of nested mathfield

--- src/core/mathfield-box.ts
+++ src/core/mathfield-box.ts
@@ -1,25 +1,72 @@
-import { Box, BoxOptions } from './box';
+import { adjustInterAtomSpacing, Box, BoxOptions, makeStruts } from './box';
 import MathfieldElement from '../public/mathfield-element';
+import { MathfieldPrivate } from 'editor/mathfield';
+import { Context } from './context';
+import { DEFAULT_FONT_SIZE } from './font-metrics';
+// import { MathfieldPrivate } from '../editor-mathfield/mathfield-private';
+// import { Context } from './context';
+// import { DEFAULT_FONT_SIZE } from './font-metrics';
+
+function makeBox(mathfield: MathfieldPrivate) {
+  const base = mathfield.model.root.render(
+    new Context(
+      {
+        registers: mathfield.registers,
+      },
+      {
+        fontSize: DEFAULT_FONT_SIZE,
+        letterShapeStyle: mathfield.options.letterShapeStyle,
+      },
+      mathfield.options.defaultMode === 'inline-math'
+        ? 'textstyle'
+        : 'displaystyle'
+    )
+  )!;
+
+  //
+  // 3. Construct struts around the boxes
+  //
+  const wrapper = makeStruts(
+    adjustInterAtomSpacing(base, mathfield.options.horizontalSpacingScale),
+    {
+      classes: 'ML__mathlive',
+      attributes: {
+        // Sometimes Google Translate kicks in an attempts to 'translate' math
+        // This doesn't work very well, so turn off translate
+        'translate': 'no',
+        // Hint to screen readers to not attempt to read this <span>.
+        // They should use instead the 'aria-label' attribute.
+        'aria-hidden': 'true',
+      },
+    }
+  );
+  return wrapper;
+}
 
 export class MathfieldBox extends Box {
-  placeholderId: string;
-  element: MathfieldElement;
+  mathfield: MathfieldElement;
+  element: MathfieldElement; // x
+
   constructor(
     placeholderId: string,
-    element: MathfieldElement,
+    mathfield: MathfieldElement,
     options?: BoxOptions
   ) {
     super(null, options);
-    this.placeholderId = placeholderId;
-    this.element = element;
-    this.htmlData = `placeholder-id=${placeholderId}`;
-    this.height =
-      element.style.fontSize === ''
-        ? 1
-        : (element.clientHeight / parseInt(element.style.fontSize)) * 0.6;
+    this.mathfield = mathfield;
+    this.htmlData = `placeholder-id=${placeholderId} `;
+
+    const box = makeBox(mathfield['_mathfield']!);
+    this.height = box.height;
+    this.depth = box.depth;
+
+    // this.height =
+    //   element.style.fontSize === ''
+    //     ? 1
+    //     : (element.clientHeight / parseInt(element.style.fontSize)) * 0.6;
   }
   toMarkup(): string {
-    let props = '';
+    const props: string[] = [];
     const classes = this.classes.split(' ');
     const classList =
       classes.length === 1
@@ -30,7 +77,7 @@ export class MathfieldBox extends Box {
 
     if (this.cssId) {
       // A (HTML5) CSS id may not contain a space
-      props += ` id=${this.cssId.replace(/ /g, '-')} `;
+      props.push(`id=${this.cssId.replace(/ /g, '-')}`);
     }
 
     if (this.htmlData) {
@@ -39,10 +86,10 @@ export class MathfieldBox extends Box {
         const matched = entry.match(/([^=]+)=(.+$)/);
         if (matched) {
           const key = matched[1].trim().replace(/ /g, '-');
-          if (key) props += ` data-${key}=${matched[2]} `;
+          if (key) props.push(`data-${key}=${matched[2]}`);
         } else {
           const key = entry.trim().replace(/ /g, '-');
-          if (key) props += ` data-${key} `;
+          if (key) props.push(`data-${key}`);
         }
       }
     }
@@ -57,21 +104,24 @@ export class MathfieldBox extends Box {
           if (key) styleString += `${key}:${matched[2]};`;
         }
       }
-      if (styleString) props += ` style="${styleString}"`;
+      if (styleString) props.push(`style="${styleString}"`);
     }
 
     if (this.attributes) {
-      props +=
-        ' ' +
-        Object.keys(this.attributes)
-          .map((x) => `${x}="${this.attributes![x]}"`)
-          .join(' ');
+      props.push(
+        ...Object.keys(this.attributes).map(
+          (x) => `${x}="${this.attributes![x]}"`
+        )
+      );
     }
 
-    if (classList.length > 0) props += ` class="${classList}"`;
+    if (classList.length > 0) props.push(`class="${classList}"`);
 
-    props += ` style="display: inline-block; width:${this.element.clientWidth}px; height:${this.element.clientHeight}px; "`;
+    const element = this.mathfield;
+    props.push(
+      `style="display:inline-block; width:${element.clientWidth}px; height:${element.clientHeight}px;" `
+    );
 
-    return `<span ${props}></span>`;
+    return `<span ${props.join(' ')}></span>`;
   }
 }
