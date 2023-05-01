32271d7aa2e3aee881c008c04ff7ba728f17a22c
Qijia Liu <liumeo@pku.edu.cn>
2023-05-01 11:13:25 -0400

    fix: switchKeyboardLayer (#1954)

--- src/virtual-keyboard/commands.ts
+++ src/virtual-keyboard/commands.ts
@@ -3,32 +3,14 @@ import { register } from '../editor/commands';
 import { hideVariantsPanel } from './variants';
 import { VirtualKeyboard } from './virtual-keyboard';
 
-export function switchKeyboardLayer(layerName: string | null): boolean {
+export function switchKeyboardLayer(layerName: string): boolean {
   const keyboard = VirtualKeyboard.singleton;
   keyboard.show();
 
   // If the variants panel was visible, hide it
   hideVariantsPanel();
 
-  const layers = keyboard.element!.querySelectorAll<HTMLElement>('.MLK__layer');
-  // Search for the requested layer
-  let found = false;
-  for (const layer of layers) {
-    if (layer.dataset.layer === layerName) {
-      found = true;
-      break;
-    }
-  }
-
-  // We did find the layer, switch to it.
-  // If we didn't find it, do nothing and keep the current layer
-  if (found) {
-    for (const layer of layers) {
-      if (layer.dataset.layer === layerName) layer.classList.add('is-visible');
-      else layer.classList.remove('is-visible');
-    }
-  }
-
+  keyboard.currentLayer = layerName;
   keyboard.render(); // Account for shift state
   keyboard.focus();
 
