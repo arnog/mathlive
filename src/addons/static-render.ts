15fe09276a8e4e5f2fbfd0c59431ffec9e599415
Arno Gourdol <arno@arno.org>
2025-07-11 14:15:39 -0700

    fix: asciimath rendering

--- src/addons/static-render.ts
+++ src/addons/static-render.ts
@@ -477,7 +477,7 @@ const DEFAULT_AUTO_RENDER_OPTIONS: StaticRenderOptions = {
 
   asciiMath: {
     delimiters: {
-      inline: ['`', '`'], // ASCII Math delimiters
+      inline: [['`', '`']], // ASCII Math delimiters
     },
   },
 
